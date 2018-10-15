d82e65ba88d6b9881553cc9caba780384da567df
Victor Rosas <Pugintv@users.noreply.github.com>
2018-10-14 22:57:58 -0700

    Add files via upload
    Commited Scripts

---
+++ Assets/Scripts/BackOcclu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackOcclu : MonoBehaviour {
+		
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+			Application.LoadLevel ("Menu 3D");
+	}
+}
---
+++ Assets/Scripts/BackVideo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackVideo : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+			Application.LoadLevel ("Menu 3D");
+	}
+}
