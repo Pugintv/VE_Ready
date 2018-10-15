84133972813e1f88270f1a07fc911fdd98778891
Victor Rosas <Pugintv@users.noreply.github.com>
2018-10-14 23:13:24 -0700

    Add files via upload

---
+++ Assets/MAINMENU/Scripts/Button.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	public void ChangeToScene (string sceneToChangeTo) {
+		Application.LoadLevel (sceneToChangeTo);
+	}
+	
+	/*void Update(){
+		if (Input.GetKeyDown(KeyCode.Escape))
+			Application.LoadLevel ("MainMenu");
+	}*/
+	
+}
